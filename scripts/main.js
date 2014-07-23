8d704cc29b688d8d1a973db0e9f7ff54387928af
Matthew Schrepel <mschrepel@gmail.com>
2014-07-23 14:23:23 -0700

    what I wanted, but ugly

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
 $(function(){
-	
+	$('input').keyup(function(){
+		var allInput = $('input')
+		var inArr = []
+		for (var i = 0; i < allInput.length; i++) {
+			inArr.push($('input:eq('+i+')').val().length)
+		};
+		if(inArr.sort()[0]===0){
+
+		} else {
+			$('p').fadeIn(750)
+		}
+	});
 });
