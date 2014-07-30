c3ec5912181b6929e21375626c0fd77bfc075bfb
Matthew Schrepel <mschrepel@gmail.com>
2014-07-30 10:36:42 -0700

    refactor 2

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+angular.module('anglibs', [])
+.controller('LibsCtrl', function($scope){
+	$scope.maleName;
+	$scope.jobTitle;
+	$scope.tediousTask;
+	$scope.dirtyJob;
+	$scope.celebrity;
+	$scope.uselessSkill;
+	$scope.obnoxiousCeleb;
+	$scope.hugeNumber;
+	$scope.adjective
+})
+
+// $(function(){
+// 	$('input').keyup(function(){
+// 		var allInput = $('input')
+// 		var inArr = []
+// 		for (var i = 0; i < allInput.length; i++) {
+// 			inArr.push($('input:eq('+i+')').val().length)
+// 		};
+// 		if(inArr.sort()[0]===0){
+
+// 		} else {
+// 			$('p').fadeIn(750)
+// 		}
+// 	});
+// });
